178611a8cabae1238902b2a7f72ae04186489815
Sayan Das <sayandasraja56@gmail.com>
2024-11-07 16:01:01 +0530

    add Sanity TypeGen

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,5 +1,5 @@
 import SearchForm from "@/components/SearchForm";
-import StartupCard from "@/components/StartupCard";
+import StartupCard, { StartupTypeCard } from "@/components/StartupCard";
 import { client } from "@/sanity/lib/client";
 import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 
@@ -13,23 +13,9 @@ export default async function Home({
 
   const query = (await searchParams).query;
 
-  // const posts = [
-  //   {
-  //     _createdAt: new Date(),
-  //     description: "Startup Idea",
-  //     views: 100,
-  //     _id: 1,
-  //     author: { _id: 1,
-  //               name: "John Doe",
-  //      },
-  //     image: "https://images.pexels.com/photos/10583179/pexels-photo-10583179.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
-  //     catagory: "Tech",
-  //     title: "Tech Startup",
 
-  //   }
-  // ]
-
-  const posts = await client.fetch(STARTUPS_QUERY)
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const posts: any = await client.fetch(STARTUPS_QUERY)
   return (
     <>
       <section className="pink_container">
@@ -53,7 +39,7 @@ export default async function Home({
         <ul className="mt-7 card_grid">
           {posts?.length > 0 ? (
             posts.map((post: StartupTypeCard) => (
-              <StartupCard key={post?._id} post={post}/>
+              <StartupCard key={post?._id} post={post} />
             ))
           ) : (
             <p className="no-results">No startups found</p>
--- src/components/StartupCard.tsx
+++ src/components/StartupCard.tsx
@@ -1,16 +1,20 @@
+/* eslint-disable @typescript-eslint/no-non-null-asserted-optional-chain */
 import { formatDate } from '@/lib/utils'
 import { EyeIcon } from 'lucide-react'
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import { Button } from './ui/button'
+import { Author, Startup } from '@/sanity/types'
+
+export type StartupTypeCard = Omit<Startup, "author"> & { author?: Author };
 
 const StartupCard = ({ post }: { post: StartupTypeCard }) => {
 
     const {
         _createdAt,
         views,
-        author: { name, _id: authorId },
+        author,
         title,
         category,
         _id,
@@ -30,17 +34,18 @@ const StartupCard = ({ post }: { post: StartupTypeCard }) => {
 
             <div className="flex-between mt-5 gap-5">
                 <div className="flex-1">
-                    <Link href={`/user/${authorId}`}>
-                        <p className="text-16-medium line-clamp-1">{name}</p>
+                    <Link href={`/user/${author?._id}`}>
+                        <p className="text-16-medium line-clamp-1">{author?.name}</p>
                     </Link>
                     <Link href={`/startup/${_id}`}>
                         <h3 className="text-26-semibold line-clamp-1">{title}</h3>
                     </Link>
                 </div>
-                <Link href={`/user/${authorId}`}>
+                <Link href={`/user/${author?._id}`}>
                     <Image
-                        src={ post.author?.image }
-                        alt={post.author?.name}
+                        // eslint-disable-next-line @typescript-eslint/no-non-null-asserted-optional-chain
+                        src={ author?.image! }
+                        alt={ author?.name! }
                         width={48}
                         height={48}
                         className="rounded-full"
@@ -51,7 +56,7 @@ const StartupCard = ({ post }: { post: StartupTypeCard }) => {
             <Link href={`/startup/${_id}`}>
         <p className="startup-card_desc">{description}</p>
 
-        <Image src= {image} alt="placeholder" className="startup-card_img" width={48}
+        <Image src= {image!} alt="placeholder" className="startup-card_img" width={48}
                         height={48}/>
       </Link>
 
