4df65bfde703a500785a62d1bbbba3b7d5512ea8
Sayan Das <sayandasraja56@gmail.com>
2024-11-07 13:39:01 +0530

    add Sanity integration with schema definitions, client setup, and startup querying.

--- src/app/(root)/page.tsx
+++ src/app/(root)/page.tsx
@@ -1,4 +1,7 @@
 import SearchForm from "@/components/SearchForm";
+import StartupCard from "@/components/StartupCard";
+import { client } from "@/sanity/lib/client";
+import { STARTUPS_QUERY } from "@/sanity/lib/queries";
 
 
 
@@ -9,6 +12,24 @@ export default async function Home({
 }) {
 
   const query = (await searchParams).query;
+
+  // const posts = [
+  //   {
+  //     _createdAt: new Date(),
+  //     description: "Startup Idea",
+  //     views: 100,
+  //     _id: 1,
+  //     author: { _id: 1,
+  //               name: "John Doe",
+  //      },
+  //     image: "https://images.pexels.com/photos/10583179/pexels-photo-10583179.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load",
+  //     catagory: "Tech",
+  //     title: "Tech Startup",
+
+  //   }
+  // ]
+
+  const posts = await client.fetch(STARTUPS_QUERY)
   return (
     <>
       <section className="pink_container">
@@ -21,7 +42,24 @@ export default async function Home({
           Submit Ideas, Vote on Pitches, and Get Noticed in Virtual
           Competitions.
         </p>
-        <SearchForm query = { query }/>
+        <SearchForm query={query} />
+      </section>
+
+      <section className="section_container">
+        <p className="text-30-semibold">
+          {query ? `Search results for "${query}"` : "All Startups"}
+        </p>
+
+        <ul className="mt-7 card_grid">
+          {posts?.length > 0 ? (
+            posts.map((post: StartupTypeCard) => (
+              <StartupCard key={post?._id} post={post}/>
+            ))
+          ) : (
+            <p className="no-results">No startups found</p>
+          )}
+        </ul>
+
       </section>
     </>
   );
